1866a232ef4e088efabea476485284590a4f0a18
Pranav <chhabrapranav2001@gmail.com>
2024-07-02 12:31:40 -0500

    Updated config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const MongoDBURL = "mongodb+srv://chhabrapranav2001:Chhabra@book-store.cuox6og.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store"
+export const MongoDBURL = "Your MongoDB URL Here";
